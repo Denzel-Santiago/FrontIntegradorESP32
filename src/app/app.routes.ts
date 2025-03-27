26aa757588c838cbf72631f770eab24b7cb83c16
Denzel-Santiago <233370@ids.upchiapas.edu.mx>
2025-03-27 00:08:45 -0600

    home y ventas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../app//home//home.component';
+import { VentasComponent } from './ventas/ventas.component';
 
 export const routes: Routes = [
 
     
     { path: '', component: HomeComponent },
+    { path: 'ventas', component: VentasComponent },
+
 
 
 
---
+++ src/app/ventas/ventas.component.ts
@@ -0,0 +1,9 @@
+/* home.component.ts */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ventas',
+  templateUrl: './ventas.component.html',
+  styleUrls: ['./ventas.component.css']
+})
+export class VentasComponent {}
